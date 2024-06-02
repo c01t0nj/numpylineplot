f20a1a6acca99fe669b85afd467c5586cd192cc0
c01t0nj <46273250+c01t0nj@users.noreply.github.com>
2024-06-02 15:34:41 -0400

    Add files via upload

---
+++ quadcurv/quadcurv.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+# create a quadratic curve
+data = np.arange(0, 10) ** 2
+plt.plot(data)
+
+# plt.show() will open the plot in its own window
+plt.show()
